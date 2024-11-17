c995169bd2a7f38610e561265202b0518ac1e266
lokka30 <lokka30@protonmail.com>
2024-11-18 07:41:53 +0800

    Economy.kt: no singleton

--- api/src/main/kotlin/io/github/arcaneplugins/polyconomy/api/Economy.kt
+++ api/src/main/kotlin/io/github/arcaneplugins/polyconomy/api/Economy.kt
@@ -1,19 +1,5 @@
 package io.github.arcaneplugins.polyconomy.api
 
-import java.util.*
-
 interface Economy {
-
-    companion object {
-        var instance: Economy? = null
-            set(value) = if(field == null) {
-                field = value
-            } else {
-                throw IllegalStateException("Economy instance is already set")
-            }
-            get() = Objects.requireNonNull(field, "Economy instance is not set yet")
-    }
-
-    
 
 }
