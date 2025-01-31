4d0e77aed10bbddd96a4e6aea58340fa6272c173
lokka30 <lokka30@protonmail.com>
2025-01-31 20:16:35 +0800

    TranslationsCfg: add opt-in for experimental kotlin api

--- plugin-core/src/main/kotlin/io/github/arcaneplugins/polyconomy/plugin/core/config/translations/TranslationsCfg.kt
+++ plugin-core/src/main/kotlin/io/github/arcaneplugins/polyconomy/plugin/core/config/translations/TranslationsCfg.kt
@@ -2,8 +2,10 @@ package io.github.arcaneplugins.polyconomy.plugin.core.config.translations
 
 import io.github.arcaneplugins.polyconomy.plugin.core.Platform
 import io.github.arcaneplugins.polyconomy.plugin.core.config.Config
+import kotlin.io.path.ExperimentalPathApi
 import kotlin.io.path.Path
 
+@OptIn(ExperimentalPathApi::class)
 class TranslationsCfg(
     plugin: Platform,
 ) : Config(
